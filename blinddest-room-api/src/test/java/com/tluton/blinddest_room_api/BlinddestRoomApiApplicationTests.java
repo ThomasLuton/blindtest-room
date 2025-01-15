55caf7e84e2867232ef25b31347bb49fe596261a
ThomasLuton <luton.thomas60@gmail.com>
2025-01-15 16:34:12 +0100

    first commit

---
+++ blinddest-room-api/src/test/java/com/tluton/blinddest_room_api/BlinddestRoomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tluton.blinddest_room_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinddestRoomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
